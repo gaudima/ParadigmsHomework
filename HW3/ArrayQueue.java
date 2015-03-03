8f7e5880d5d73c1a7347b0fc3a7728bd47f8a4c1
gaudima <gaudima@gmail.com>
2015-03-03 14:59:54 +0400

    added contracts

--- HW3/ArrayQueue.java
+++ HW3/ArrayQueue.java
@@ -9,7 +9,7 @@ public class ArrayQueue {
         if (capacity > len) {
             Object[] newElements = new Object[elements.length * 2];
             int i = 0;
-            while(last!=first) {
+            while (last!=first) {
                 newElements[i] = elements[first];
                 first = (first + 1) % len;
                 i++;
@@ -20,6 +20,10 @@ public class ArrayQueue {
         }
     }
 
+    //pre: element != null
+    //post: elements[last] = element,
+    //      last = (last + 1) % elements.length,
+    //      size = size + 1 
     public void enqueue(Object element) {
         assert element != null;
         enshureCapacity(size + 2);
@@ -29,11 +33,18 @@ public class ArrayQueue {
         //System.out.println(elements.length);
     }
 
+    
+    //pre: size > 0
+    //post: R = elements[first]
     public Object element() {
         assert size > 0;
         return elements[first];
     }
 
+    //pre: size > 0
+    //post: R = elements[first],
+    //      first = (first + 1) % elements.length,
+    //      size = size - 1 
     public Object dequeue() {
         Object ret = element();
         first = (first + 1) % elements.length;
@@ -41,17 +52,61 @@ public class ArrayQueue {
         return ret;
     }
 
+    //post: R = size
     public int size() {
         return size;
     }
 
+    //post: R = (size == 0)
     public boolean isEmpty() {
         return (size == 0);
     }
 
+    //post: size = 0,
+    //      first = 0,
+    //      last = 0,
     public void clear() {
         first = 0;
         last = 0;
         size = 0;
+    }
+
+    //pre: element != null
+    //post: (first - 1 < 0) ? first = elements.length - 1 : first = first - 1,
+    //      elements[first] = element,
+    //      size = size + 1;
+    public void push(Object element) {
+        assert element != null;
+        enshureCapacity(size + 2);
+        first = first - 1;
+        if (first < 0) {
+            first = elements.length - 1;
+        }
+        elements[first] = element;
+        size++;
+    }
+
+    //pre: size > 0;
+    //post: R = elements[(last - 1 < 0) ? elements.length - 1 : last - 1]
+    public Object peek() {
+        assert size > 0;
+        int llast = last - 1;
+        if (llast < 0) {
+            llast = elements.length - 1;
+        }
+        return elements[llast];
+    }
+
+    //pre: size > 0;
+    //post: (last - 1 < 0) ? last = elements.length - 1 : last = last - 1,
+    //      R = elements[last]   
+    public Object remove() {
+        assert size > 0;
+        last = last - 1;
+        if (last < 0) {
+            last = elements.length - 1;
+        }
+        size--;
+        return elements[last];
     }
 }
--- HW3/ArrayQueueADT.java
+++ HW3/ArrayQueueADT.java
@@ -5,11 +5,12 @@ public class ArrayQueueADT {
     private Object[] elements = new Object[10];
 
     static void enshureCapacity(ArrayQueueADT queue, int capacity) {
+        assert queue != null;
         int len = queue.elements.length;
         if (capacity > len) {
             Object[] newElements = new Object[queue.elements.length * 2];
             int i = 0;
-            while(queue.last!=queue.first) {
+            while (queue.last!=queue.first) {
                 newElements[i] = queue.elements[queue.first];
                 queue.first = (queue.first + 1) % len;
                 i++;
@@ -20,7 +21,13 @@ public class ArrayQueueADT {
         }
     }
 
+    //pre: element != null,
+    //     queue != null
+    //post: elements[last] = element,
+    //      last = (last + 1) % elements.length,
+    //      size = size + 1
     public static void enqueue(ArrayQueueADT queue, Object element) {
+        assert queue != null;
         assert element != null;
         enshureCapacity(queue, queue.size + 2);
         queue.elements[queue.last] = element;
@@ -29,29 +36,95 @@ public class ArrayQueueADT {
         //System.out.println(elements.length);
     }
 
+    //pre: queue != null,
+    //     size > 0
+    //post: R = elements[first]
     public static Object element(ArrayQueueADT queue) {
+        assert queue != null;
         assert queue.size > 0;
         return queue.elements[queue.first];
     }
 
+    //pre: queue != null,
+    //     size > 0
+    //post: R = elements[first],
+    //      first = (first + 1) % elements.length,
+    //      size = size - 1
     public static Object dequeue(ArrayQueueADT queue) {
+        assert queue != null;
         Object ret = element(queue);
         queue.first = (queue.first + 1) % queue.elements.length;
         queue.size--;
         return ret;
     }
 
+    //pre: queue != null
+    //post: R = size
     public static int size(ArrayQueueADT queue) {
+        assert queue != null;
         return queue.size;
     }
 
+    //pre: queue != null
+    //post: R = (size == 0)
     public static boolean isEmpty(ArrayQueueADT queue) {
+        assert queue != null;
         return (queue.size == 0);
     }
 
+    //pre: queue != null
+    //post: size = 0,
+    //      first = 0,
+    //      last = 0
     public static void clear(ArrayQueueADT queue) {
+        assert queue != null;
         queue.first = 0;
         queue.last = 0;
         queue.size = 0;
+    }
+
+    //pre: queue != null,
+    //     element != null
+    //post: (first - 1 < 0) ? first = elements.length - 1 : first = first - 1,
+    //      elements[first] = element,
+    //      size = size + 1;
+    public static void push(ArrayQueueADT queue, Object element) {
+        assert queue != null;
+        assert element != null;
+        enshureCapacity(queue, queue.size + 2);
+        queue.first = queue.first - 1;
+        if (queue.first < 0) {
+            queue.first = queue.elements.length - 1;
+        }
+        queue.elements[queue.first] = element;
+        queue.size++;
+    }
+
+    //pre: queue != null,
+    //     size > 0;
+    //post: R = elements[(last - 1 < 0) ? elements.length - 1 : last - 1]
+    public static Object peek(ArrayQueueADT queue) {
+        assert queue != null;
+        assert queue.size > 0;
+        int llast = queue.last - 1;
+        if (llast < 0) {
+            llast = queue.elements.length - 1;
+        }
+        return queue.elements[llast];
+    }
+
+    //pre: queue!= null,
+    //     size > 0;
+    //post: (last - 1 < 0) ? last = elements.length - 1 : last = last - 1,
+    //      R = elements[last]   
+    public static Object remove(ArrayQueueADT queue) {
+        assert queue != null;
+        assert queue.size > 0;
+        queue.last = queue.last - 1;
+        if (queue.last < 0) {
+            queue.last = queue.elements.length - 1;
+        }
+        queue.size--;
+        return queue.elements[queue.last];
     }
 }
