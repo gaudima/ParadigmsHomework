c9f9c6f40cbd6c7ce93af43803ce8bc8dba175c7
gaudima <gaudima@gmail.com>
2015-03-10 14:38:30 +0400

    added Queue interface contracts

--- HW4/Queue.java
+++ HW4/Queue.java
@@ -2,20 +2,33 @@ import java.util.function.Predicate;
 import java.util.function.Function;
 
 public interface Queue {
-    
+    //pre: element != null
+    //post: size = size + 1,
+    //      element inserted at the end of queue
     public void enqueue(Object element);
 
+    //pre: size > 0
+    //post: R = first element in queue;
     public Object element();
 
+    //pre: size > 0;
+    //post: size = size - 1,
+    //      first element of queue deleted
     public Object dequeue();
 
+    //post: R = size
     public int size();
 
+    //post: R = (size == 0)
     public boolean isEmpty();
 
+    //post: size = 0
+    //      delete all queue elements
     public void clear();
 
+    //post: R = queue of elements of class instance matching predicate
     public Queue filter(Predicate<Object> predicate);
 
+    //post: R = queue of elements of class instance with func applied to them
     public Queue map(Function<Object, Object> func);
 }
