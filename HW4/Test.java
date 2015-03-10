15413acb12764d3827c28e947e628f6638aa89c8
gaudima <gaudima@gmail.com>
2015-03-10 12:08:51 +0400

    removed Test

--- HW4/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        LinkedQueue queue = new LinkedQueue();
-        queue.enqueue("Hello");
-        queue.push("World");
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-    }
-}
