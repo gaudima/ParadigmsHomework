59c9c4cd1f2473fc50970359266f368a4e1c7b65
gaudima <gaudima@gmail.com>
2015-03-17 17:16:21 +0400

    some improvements

--- HW5/TripleExpression/Add.java
+++ HW5/TripleExpression/Add.java
@@ -2,11 +2,10 @@ package TripleExpression;
 
 public class Add extends BinaryOperator implements TripleExpression {
     public Add(TripleExpression first, TripleExpression second) {
-        fOp = first;
-        sOp = second;
+        super(first, second);
     }
 
-    public double evaluate(double x, double y , double z) {
-        return fOp.evaluate(x ,y ,z) + sOp.evaluate(x, y, z);
+    protected double apply(double a, double b) {
+        return a + b;
     }
 }
--- HW5/TripleExpression/BinaryOperator.java
+++ HW5/TripleExpression/BinaryOperator.java
@@ -4,5 +4,14 @@ abstract public class BinaryOperator implements TripleExpression {
     protected TripleExpression fOp;
     protected TripleExpression sOp;
     
-    abstract public double evaluate(double x, double y, double z);
+    BinaryOperator(TripleExpression first, TripleExpression second) {
+        fOp = first;
+        sOp = second;
+    }
+
+    abstract protected double apply(double a, double b);
+    
+    public double evaluate(double x, double y, double z) {
+        return apply(fOp.evaluate(x, y, z), sOp.evaluate(x, y, z));
+    }
 }
