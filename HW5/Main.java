6f9f9f280b83afee73c453175aacb790af812519
gaudima <gaudima@gmail.com>
2015-03-17 09:44:12 +0400

    added Main class

--- HW5/Main.java
+++ HW5/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(new Subtract(new Multiply(new Const(2), new Variable("x")), new Const(3)).evaluate(5));
+        assert args.length >= 1;
+        double arg = Double.parseDouble(args[0]);
+		System.out.println(new Add(new Subtract(new Multiply(new Variable("x"), new Variable("x")), new Multiply(new Const(2), new Variable("x"))), new Const(1)).evaluate(arg));
 	}
 }
